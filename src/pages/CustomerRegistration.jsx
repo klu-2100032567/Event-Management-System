22d5216461a741d428aa6b790f413a834acbf4d0
klu-2100032567 <2100032567@kluniversity.in>
2024-06-10 10:15:40 +0530

    intial commit

---
+++ src/pages/CustomerRegistration.jsx
@@ -0,0 +1,104 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import { Container, Row, Col, Form, FormGroup, Input } from "reactstrap";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/CommonSection";
+
+import "../styles/contact.css";
+const socialLinks = [
+  {
+    url: "#",
+    icon: "ri-facebook-line",
+  },
+  {
+    url: "#",
+    icon: "ri-instagram-line",
+  },
+  {
+    url: "#",
+    icon: "ri-linkedin-line",
+  },
+  {
+    url: "#",
+    icon: "ri-twitter-line",
+  },
+];
+
+const Register = () => {
+  return (
+    <Helmet title="Contact">
+      <CommonSection title="Customer Registration" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="7" md="7">
+              <h6 className="fw-bold mb-4">Get In Touch With Us</h6>
+
+              <Form>
+                <FormGroup className="contact__form">
+                  <Input placeholder="Your Name First Name" type="text" required/>
+                </FormGroup>
+                <FormGroup className="contact__form">
+                  <Input placeholder="Your Name Middle Name" type="text" required/>
+                </FormGroup>
+                <FormGroup className="contact__form">
+                  <Input placeholder="Your Name Last Name" type="text" required/>
+                </FormGroup>
+                <FormGroup className="contact__form">
+                  <Input placeholder="Email" type="email" required/>
+                </FormGroup>
+                <FormGroup className="contact__form">
+                  <Input placeholder="Contact Number" type="text" required/>
+                </FormGroup>
+                <FormGroup className="contact__form">
+                  <Input placeholder="Address" type="text" required/>
+                </FormGroup>
+
+                <button className=" contact__btn" type="submit">
+                <Link to="/home">Confrim</Link>
+                </button>
+                <button className=" contact__btn" type="submit">
+                <Link to="/eventmanagerregistration">Vendor Register</Link>
+  </button>
+              </Form>
+            </Col>
+
+            <Col lg="5" md="5">
+              <div className="contact__info">
+                <h6 className="fw-bold">Contact Information</h6>
+                <p className="section__description mb-0">
+                  Vaddeswaram,Vijayawada.
+                </p>
+                <div className=" d-flex align-items-center gap-2">
+                  <h6 className="fs-6 mb-0">Phone:</h6>
+                  <p className="section__description mb-0">+91 9985842229</p>
+                </div>
+
+                <div className=" d-flex align-items-center gap-2">
+                  <h6 className="mb-0 fs-6">Email:</h6>
+                  <p className="section__description mb-0">eventmanagement@gmail.com</p>
+                </div>
+
+                <h6 className="fw-bold mt-4">Follow Us</h6>
+
+                <div className=" d-flex align-items-center gap-4 mt-3">
+                  {socialLinks.map((item, index) => (
+                    <Link
+                      to={item.url}
+                      key={index}
+                      className="social__link-icon"
+                    >
+                      <i class={item.icon}></i>
+                    </Link>
+                  ))}
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default Register;
